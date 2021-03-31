3a9b4c4f0007e79b91b4d30a52a7318176647d82
Luis Vazquez <luvazpa@gmail.com>
2021-03-31 17:59:45 -0600

    Eslint added

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 
-app.get("/", (req, res) => {
+let notes = [
+  {
+    id: 1,
+    content: 'Repasar los retos de JS de midudev',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Repasar los retos de JS de midudev',
+    important: true
+  },
+  {
+    id: 3,
+    content: 'Repasar los retos de JS de midudev',
+    important: true
+  }
+];
+
+app.get('/', (req, res) => {
   res.json({
-    hola: "mundo",
+    hola: 'mundo desde index'
   });
 });
 
-app.listen(3000, () => console.log(`Server running on port 3000`));
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+  const note = notes.find((note) => note.id === parseInt(id));
+  res.json(note);
+});
+
+app.post('/api/notes', (req, res) => {
+  const note = req.body;
+
+  const ids = notes.map((note) => parseInt(note.id));
+  const id = Math.max(...ids) + 1;
+  const newNote = {
+    id,
+    content: note.content,
+    important: note.important,
+    date: new Date().toISOString()
+  };
+  notes.push(newNote);
+
+  res.status(201).json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+  notes = notes.filter((note) => note.id !== parseInt(id));
+  res.status(204).end();
+});
+
+app.listen(3000, () => console.log(`Server running on port ${port}`));
